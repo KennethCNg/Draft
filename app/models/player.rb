d6aefba4f9f905ca6d64e8d7f1a4bfb733d5caf2
Kenneth Ng <kenneth.ng.4@gmail.com>
2018-03-23 06:44:28 -0400

    add annotated models

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: players
+#
+#  id         :integer          not null, primary key
+#  first_name :string
+#  last_name  :string
+#  position   :string
+#  age        :integer
+#  sport_id   :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Player < ApplicationRecord
+    
 end
--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: sports
+#
+#  id          :integer          not null, primary key
+#  sport_name  :string           not null
+#  average_age :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Sport < ApplicationRecord
 end
