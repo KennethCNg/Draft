d63d49e5d359c9c22f5d1705301db38959ed4066
Kenneth Ng <kenneth.ng.4@gmail.com>
2018-03-23 07:44:13 -0400

    finish routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    get 'sports/:sport_name', to: 'sports#index'
+  end
 end
