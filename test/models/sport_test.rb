4d457a2cb2d9ea6248f76b80b66cb0d6ba81289c
Kenneth Ng <kenneth.ng.4@gmail.com>
2018-03-24 02:43:34 -0400

    push autogenerated tests

--- test/models/sport_test.rb
+++ test/models/sport_test.rb
@@ -2,11 +2,10 @@
 #
 # Table name: sports
 #
-#  id          :integer          not null, primary key
-#  sport_name  :string           not null
-#  average_age :integer          not null
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id         :integer          not null, primary key
+#  sport_name :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
 #
 
 require 'test_helper'
