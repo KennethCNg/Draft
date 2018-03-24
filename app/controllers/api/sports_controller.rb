6ab1017ef88eaee7e74c84e61612ea17b8b0bb0c
Kenneth Ng <kenneth.ng.4@gmail.com>
2018-03-24 05:53:18 -0400

    refactor controller to use avg_age concern

--- app/controllers/api/sports_controller.rb
+++ app/controllers/api/sports_controller.rb
@@ -1,5 +1,6 @@
 class Api::SportsController < ApplicationController
     def index
+        # calls the methods in the AvgAgeByPos Concern
         sport, players = Sport.find_sport_and_players(params[:sport_name])
         avg_age_hash = sport.calculate_avg_age_by_position(players)
         json_obj = sport.create_json_response(players, avg_age_hash)
