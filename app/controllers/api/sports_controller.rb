fb7cc94c27a5a9a1b4d8d166aeba201990893fa6
Kenneth Ng <kenneth.ng.4@gmail.com>
2018-03-24 03:38:45 -0400

    finish controller

--- app/controllers/api/sports_controller.rb
+++ app/controllers/api/sports_controller.rb
@@ -1,8 +1,10 @@
 class Api::SportsController < ApplicationController
     def index
-        @average_age, @players = Sport.find_sports_players(params[:sport_name])
+        sport, players = Sport.find_sport_and_players(params[:sport_name])
+        avg_age_hash = sport.calculate_avg_age_by_position(players)
+        json_obj = sport.create_json_response(players, avg_age_hash)
 
-        render :index
+        render json: json_obj
     end
 
 end
