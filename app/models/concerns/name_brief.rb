8566c8c9235d53a8bcf9f7201b4fba7e61f0c2f2
Kenneth Ng <kenneth.ng.4@gmail.com>
2018-03-24 05:52:27 -0400

    refactor name brief to use procs and dictionary for faster lookup time

--- app/models/concerns/name_brief.rb
+++ app/models/concerns/name_brief.rb
@@ -2,21 +2,15 @@ module NameBrief
     extend ActiveSupport::Concern
 
     included do
-        NAME_BRIEF_STORE = [
-            "Basketball", 
-            "Baseball", 
-            "Football"
-        ]
+        NAME_BRIEF_DICTIONARY = {
+            "Basketball" => Proc.new { |player| "#{player.first_name} #{player.last_name[0]}." },
+            "Baseball" => Proc.new { |player| "#{player.first_name[0]}. #{player.last_name[0]}." },
+            "Football" => Proc.new { |player| "#{player.first_name[0]}. #{player.last_name}" }
+        }.freeze
 
         def name_brief
-            case NAME_BRIEF_STORE[self.sport_id - 1]
-            when "Basketball"
-                "#{self.first_name} #{self.last_name[0]}"
-            when "Baseball"
-                "#{self.first_name[0]} #{self.last_name[0]}"
-            when "Football"
-                "#{self.first_name[0]} #{self.last_name}"
-            end
+            name_brief_proc = NAME_BRIEF_DICTIONARY[self.sport.sport_name]
+            name_brief_proc.call(self)
         end 
     end
 end
